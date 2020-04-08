2a123ff443eeae8fdea5a04c649a66f3c9e89cd3
Warren Uhrich <hello@warren.codes>
2020-04-08 11:35:44 -0600

    Output heading component.

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
-class Heading extends React.Component {
+export class Heading extends React.Component {
     render ()
     {
         return <h1>Our First Component</h1>
